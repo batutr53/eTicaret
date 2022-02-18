dda1dbf580b95364f06050b8ed6945bb88cef15e
TRSURE <batuhanturk34@gmail.com>
2022-02-18 16:52:24 +0300

    Api ProductController

---
+++ eTicaret.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,28 @@
+﻿using eTicaret.Core.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTicaret.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode==204)//nocontent
+            
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+
+                return new ObjectResult(response)
+                {
+                    StatusCode = response.StatusCode
+                };
+            
+        }
+    }
+}
