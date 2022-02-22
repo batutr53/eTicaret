5087956d69cf5573776558677720d96e5176c7d6
TRSURE <batuhanturk34@gmail.com>
2022-02-22 14:47:33 +0300

    ProductFeature

---
+++ eTicaret.Data/Repositories/ProductFeatureRepository.cs
@@ -0,0 +1,17 @@
+﻿using eTicaret.Core;
+using eTicaret.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicaret.Repository.Repositories
+{
+    public class ProductFeatureRepository : GenericRepository<ProductFeature>, IProductFeatureRepository
+    {
+        public ProductFeatureRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
