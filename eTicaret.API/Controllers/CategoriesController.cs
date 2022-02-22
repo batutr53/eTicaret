6af01dc16bbf7e127b879055c4422703004cb416
TRSURE <batuhanturk34@gmail.com>
2022-02-22 16:13:04 +0300

    CategoryController Added

--- eTicaret.API/Controllers/CategoriesController.cs
+++ eTicaret.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using eTicaret.API.Filters;
+﻿using AutoMapper;
+using eTicaret.API.Filters;
+using eTicaret.Core;
+using eTicaret.Core.DTOs;
 using eTicaret.Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,20 +10,65 @@ namespace eTicaret.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-   
+
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
-
+        [ServiceFilter(typeof(NotFoundFilter<Category>))]
         [HttpGet("[action]/{categoryId}")]
         public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
         {
             return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var categories = await _categoryService.GetAllAync();
+            var categoriesDtos = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoriesDtos));
+        }
+
+        [ServiceFilter(typeof(NotFoundFilter<Category>))]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIAsync(id);
+            var categoriesDto = _mapper.Map<CategoryDto>(category);
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(200, categoriesDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDto categoryDto)
+        {
+            var category = await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+            var categoriesDto = _mapper.Map<CategoryDto>(category);
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(201, categoriesDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CategoryDto categoryDto)
+        {
+            await _categoryService.UpdateAsync(_mapper.Map<Category>(categoryDto));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        [ServiceFilter(typeof(NotFoundFilter<Category>))]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id) 
+        {
+        var category = await _categoryService.GetByIAsync(id);
+            await _categoryService.RemoveAsync(category);
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(200));
+        }
+
+
     }
 }
